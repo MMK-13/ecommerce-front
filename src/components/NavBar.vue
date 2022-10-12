3e169a3a5ab5ff6c7b15251438249348b778371f
MMK-13 <kantemamadou13@gmail.com>
2022-10-12 20:43:51 +0000

    Update NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,11 @@
 <template>
 	<nav class="navbar navbar-dark bg-primary">
 		<div class="container-fluid">
-			<a class="navbar-brand" href="#">
-				PC Components
-			</a>
+			<a class="navbar-brand" href="#">Navbar</a>
+			<div class="d-flex flex-row navbar-nav">
+				<a class="nav-link active" aria-current="page" href="/">Accueil</a>
+				<a class="nav-link" href="/store">Boutique</a>
+			</div>
 		</div>
 	</nav>
 </template>
@@ -13,3 +15,9 @@
 		name: 'NavBar',
 	}
 </script>
+
+<style scoped>
+	.nav-link {
+		margin-left: 8px;
+	}
+</style>
