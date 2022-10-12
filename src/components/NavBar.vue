4fe483a9c3f988288d43d4903807830282959320
MMK-13 <kantemamadou13@gmail.com>
2022-10-12 19:42:06 +0000

    Initial commit final

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+	<nav class="navbar navbar-dark bg-primary">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">
+				PC Components
+			</a>
+		</div>
+	</nav>
+</template>
+
+<script>
+	export default {
+		name: 'NavBar',
+	}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		Home test
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'HomeView',
+		components: {
+			
+		},
+	}
+</script>
