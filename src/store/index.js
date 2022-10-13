d2e8621ba721be95197bfb796890340007396353
MMK-13 <kantemamadou13@gmail.com>
2022-10-13 19:15:08 +0000

    Update Vuex for fetching product list

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,41 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+	state: {
+		products: [],
+		cart: [],
+	},
+	getters: {
+		products(state) {
+			return state.products;
+		},
+	},
+	mutations: {
+		setProducts(state, products) {
+			state.products = products
+		},
+		addToCart(state, product) {
+			const exist = state.cart.find(element => element.id == product.id)
+			if (!exist) {
+				state.cart.push({
+					id: product.id,
+					title: product.title,
+					image: product.image,
+					price: product.price,
+					quantity: 1
+				})
+			}
+		},
+	},
+	actions: {
+		fetchProductsList(context) {
+			fetch('http://ecom/product/list')
+			.then((response) => response.json())
+			.then((data) => {
+				context.commit('setProducts', data)
+			}).catch((error) => console.log(error))
+		},
+	},
+	modules: {
+	}
 })
