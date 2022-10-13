17fb56778404d13855383103ea58660390eb34c2
MMK-13 <kantemamadou13@gmail.com>
2022-10-13 19:13:23 +0000

    Create ProductCard and update HomeView

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="card mb-2" style="width: 18rem;">
+		<img src="@/assets/logo.png" class="card-img-top" alt="...">
+		<div class="card-body">
+			<h5 class="card-title">{{ title }}</h5>
+			<p class="card-text">{{ description }}</p>
+		</div>
+		<ul class="list-group list-group-flush">
+			<li class="list-group-item">Prix : {{ price }}</li>
+			<li class="list-group-item">Catégorie : {{ category }}</li>
+			<li class="list-group-item">A third item</li>
+		</ul>
+		<div class="card-body">
+			<button class="btn btn-primary" @click="addToCart()">
+				Ajouter
+			</button>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'productCard',
+		props: ['id', 'title', 'description', 'price', 'category', 'image'],
+		methods: {
+			addToCart() {
+				this.$store.commit('addToCart', {
+					id: this.id,
+					title: this.title,
+					image: this.image,
+					price: this.price,
+				})
+			}
+		}
+	}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,37 @@
 <template>
-	<div>
-		Home test
+	<div class="container mt-4">
+		<div class="row">
+			<div class="col" v-for="product in products" :key="product.id">
+				<ProductCard
+				:id="product.id"
+				:image="product.image"
+				:title="product.title"
+				:description="product.description"
+				:price="product.price"
+				:category="product.category"
+				/>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
+	import { mapActions, mapGetters } from 'vuex'
+	import ProductCard from '@/components/ProductCard'
+
 	export default {
 		name: 'HomeView',
+		created() {
+			this.fetchProductsList()
+		},
 		components: {
-			
+			ProductCard,
+		},
+		methods: {
+			...mapActions(["fetchProductsList"]),
+		},
+		computed: {
+			...mapGetters(["products"]),
 		},
 	}
 </script>
